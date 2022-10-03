8a3467b0ad57f48cc70c758a8e6eb3d833c6299d
NoobleMcScrunchkin <kieranaslett@gmail.com>
2022-10-03 02:31:34 +0100

    EEEEEE

--- build/util/handleError.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.displayError = void 0;
-function displayError(errorMessage) {
-    console.log(errorMessage);
-}
-exports.displayError = displayError;

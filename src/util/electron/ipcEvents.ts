72eaa3f3085606867e67a41edfcfe0089c4460f0
NoobleMcScrunchkin <kieranaslett@gmail.com>
2022-10-07 16:16:18 +0100

    Fix small oopsie

--- src/util/electron/ipcEvents.ts
+++ src/util/electron/ipcEvents.ts
@@ -4,7 +4,7 @@ import { InstanceManager } from "../minecraft/game/instanceManager";
 import { startGame } from "../minecraft/game/launcher";
 
 ipcMain.on("CREATE_INSTANCE", async (event, arg): Promise<void> => {
-	let instance = InstanceManager.createInstance(arg.name, arg.type, arg.version);
+	let instance = await InstanceManager.createInstance(arg.name, arg.type, arg.version);
 	let instances = InstanceManager.getInstances();
 	event.sender.send("GET_INSTANCES", { instances });
 });
